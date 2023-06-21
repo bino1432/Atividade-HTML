39a3e8b858e8a81c943323ed43c2c86cf556d5c9
bino1432 <bernardozplz@gmail.com>
2023-06-20 21:50:04 -0300

    ATT

--- Site-JS/MinhasTarafas/dom-tarefas-base/js/script.js
+++ Site-JS/MinhasTarafas/dom-tarefas-base/js/script.js
@@ -8,40 +8,64 @@ let listaTarefa = document.querySelector('#lista-tarefas');
 //     alert('clicado (GetElementById)');
 // }
 
-// evento pelo addEventListener
-botaoAdd.addEventListener('click', adicionarTarefa); 
+let itensSalvos = JSON.parse(localStorage.getItem('itens')) || []; 
 
-// Adicionando o atalho "Enter"
-campoTarefa.addEventListener('keyup', function(enter) {
-    if(enter.key === 'Enter'){
-        adicionarTarefa();
-    }
+itensSalvos.forEach(adicionarTarefa);
+
+// evento pelo addEventListener
+botaoAdd.addEventListener('click', function () {
+    
+    let tarefaNome = campoTarefa.value;
+
+    adicionarTarefa(tarefaNome)
+
+    itensSalvos.push(tarefaNome);
+
+    localStorage.setItem('itens', JSON.stringify(itensSalvos));
+    campoTarefa.value = '';
+
 });
 
-// function para adicionar uma tarefa
-function adicionarTarefa() {
+// Adicionando o atalho "Enter"
+campoTarefa.addEventListener('keyup', function (enter) {
     
+    let tarefaNome = campoTarefa.value;
+
+    if (enter.key === 'Enter') {
+        adicionarTarefa(tarefaNome)
+
+    itensSalvos.push(tarefaNome);
+
+    localStorage.setItem('itens', JSON.stringify(itensSalvos));
+    campoTarefa.value = '';
+    }
+    
+});
+
+
+// function para adicionar uma tarefa
+function adicionarTarefa(tarefaNome) {
+
     // alerta em cima da pagina
     // alert('Tarefa Adicionada!');
-    
+
     // pegando o valor do campo de texto
-    let nomeTarefa = campoTarefa.value; 
-    
+    let nomeTarefa = tarefaNome;
     // adicionando um novo elemento LI   
     let novaTarefa = document.createElement('li');
-    
+
     // definindo o conteúdo do elemento li
     novaTarefa.innerText = nomeTarefa;
-   
+
     // Adicionar o item da lista ao final da lista
     listaTarefa.appendChild(novaTarefa);
 
     // Adicionar um ouvinte ao item da lista
-    novaTarefa.addEventListener('click', function() {
+    novaTarefa.addEventListener('click', function () {
         // Quando o item da lista é clicado adicionamos a classe 'concluido'
         novaTarefa.classList.toggle("concluida");
     });
-    
+
     // Removendo Elementos
     // Criar botao excluir
     let botaoExcluir = document.createElement("button");
@@ -55,8 +79,8 @@ function adicionarTarefa() {
     novaTarefa.appendChild(botaoExcluir);
 
     // Adicionar um ouvinte ao botão excluir
-    botaoExcluir.addEventListener('click', function() {
-        
+    botaoExcluir.addEventListener('click', function () {
+
         // Deleta a tarefa da lista
         listaTarefa.removeChild(novaTarefa);
 
@@ -70,3 +94,19 @@ function adicionarTarefa() {
     // Focar no campo de texto
     campoTarefa.focus();
 };
+
+// tema escuro
+let tema = document.getElementById('btntema');
+
+document.body.className = localStorage.getItem('modo') || 'modo-claro';
+
+tema.addEventListener('click', function () {
+    if (document.body.className === 'modo-claro') {
+        document.body.className = 'modo-escuro'
+    } else {
+        document.body.className = 'modo-claro'
+    }
+
+    localStorage.setItem('modo', document.body.className);
+
+});
