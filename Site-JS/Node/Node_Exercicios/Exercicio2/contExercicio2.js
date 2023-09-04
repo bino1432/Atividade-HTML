e47fedcbf4a5f6d1ce253d30a6660cd08e9a82de
bino1432 <bernardozplz@gmail.com>
2023-09-04 19:40:58 -0300

    node

---
+++ Site-JS/Node/Node_Exercicios/Exercicio2/contExercicio2.js
@@ -0,0 +1,6 @@
+const conta = require('./exercicio2')
+const a = [5,5,5];
+
+const resultado = conta.media();
+
+console.log("a media é: ", resultado);
--- Site-JS/Node/Node_Exercicios/Exercicio2/exercicio2.js
+++ Site-JS/Node/Node_Exercicios/Exercicio2/exercicio2.js
@@ -1,5 +1,16 @@
-function media(){
+function media(a){
+    
+    let soma = 0
+    let tamanho = 0
 
+    for(let cont = 0; cont <= a.lenght; cont++){
+        soma = soma + a[cont]; 
+        tamanho++;
+    }
+    
+    let media = soma / tamanho
+
+    return media;
 }
 
 module.exports.media = media;
