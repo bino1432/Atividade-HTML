6c6a2409b8fa6197d6007c69c7a340618e496d11
bino1432 <felipe_t_maciel@estudante.sesisenai.org.br>
2023-09-04 20:29:47 -0300

    node

---
+++ Site-JS/Node/Node_Exercicios/Exercicio5/Exercicio5.js
@@ -0,0 +1,20 @@
+function soma(a,b){
+    return a+b
+}
+
+function subtracao(a,b){
+    return a-b
+}
+
+function divisao(a,b){
+    return a / b
+}
+
+function multiplicacao(a,b){
+    return a * b
+}
+
+module.exports.soma = soma
+module.exports.subtracao = subtracao
+module.exports.multiplicacao = multiplicacao
+module.exports.divisao = divisao
---
+++ Site-JS/Node/Node_Exercicios/Exercicio5/contExercicio5.js
@@ -0,0 +1,11 @@
+const conta = require('./Exercicio5')
+
+const soma = conta.soma(4,5.6)
+const subtracao = conta.subtracao(5.6,4)
+const divisao = conta.divisao(12,3)
+const multiplicacao = conta.multiplicacao(4,3)
+
+console.log("a soma é ", soma)
+console.log("a subtracao é ", subtracao)
+console.log("a divisao é ", divisao)
+console.log("a multiplicacao é ", multiplicacao)
